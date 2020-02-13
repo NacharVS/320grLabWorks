c7aebe8e077c6a8b6e35931ae8e2eb1c31c5339e
RazilIlvina <samigullin01@inbox.ru>
2020-02-13 10:59:53 +0300

    Добавил персонажа

---
+++ Lab1/RazilLab1/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassEr
+{
+     class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string ClassCharacter { get; set; }
+        public string Speciality { get; set; }
+        public int LvL { get; set; }
+
+        public Character(int id, string name, string race, string classCharacter, string speciality, int lvl)
+        {
+            Id = id;
+            Name = name;
+            Race = race;
+            ClassCharacter = classCharacter;
+            Speciality = speciality;
+            LvL = lvl;
+        }
+
+        public virtual void displayCharacter()
+        {
+            Console.WriteLine($"Идентификатор: {Id}, \nИмя: {Name}, \nРасса: {Race}, \nКласс: {ClassCharacter}, \nСпециализация: {Speciality}, \nУровень: {LvL}. \n");
+        }
+
+    }
+}
