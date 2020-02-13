7c7fe3ac13a1179ce6b2e7b1dc41be16ec3006ca
alnkmrt <aln.kmrtnv@gmail.com>
2020-02-13 10:42:37 +0300

    lab2 task's

---
+++ Lab1/Origin/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Account
+    {
+        int id;
+        string  nickname;
+        string email;
+        string login;
+        string password;
+        int number;
+        int cash;
+        int serialNum;
+        int passportNum;
+
+        public string Nickname { get => nickname; set => nickname = value; }
+        public string Email { get => email; set => email = value; }
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+        public int Number{ get => number; set => number = value; }
+        public int Cash { get => cash; set => cash = value; }
+        public int SerialNum{ get => serialNum; set => serialNum = value; }
+        public int PassportNum { get => passportNum; set => passportNum = value; }
+        public int Id { get => id; set => id = value; }
+
+        public Account (int id,string nickname, string email, string login, string password, int numer, int cash, int serialNum, int passportNum)
+        {
+            this.id = id;
+            this.nickname = nickname;
+            this.email = email;
+            this.login = login;
+            this.password = password;
+            this.number = numer;
+            this.cash = cash;
+            this.serialNum = serialNum;
+            this.passportNum = passportNum;
+        }
+    }
+}
