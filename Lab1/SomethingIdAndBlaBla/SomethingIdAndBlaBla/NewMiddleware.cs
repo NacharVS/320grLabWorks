be4fbf5a355a1286a96736a05f150ebec9b266d9
My name <ermeeva_r@mail.ru>
2020-04-23 22:15:33 +0300

    Проверка айдишника на корректность

---
+++ Lab1/SomethingIdAndBlaBla/SomethingIdAndBlaBla/NewMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SomethingIdAndBlaBla
+{
+    public class NewMiddleware
+    {
+        // Задаем запрос следующего элемента в конвеере
+        private RequestDelegate request;
+
+        // Диапозон айдишника
+        private int minId, maxId;
+
+        // Инициализация
+        public NewMiddleware(RequestDelegate request, int minId, int maxId)
+        {
+            this.request = request;
+            this.minId = minId;
+            this.maxId = maxId;
+        }
+
+        // Метод где запрос обрабатывается
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Ловим айдишник
+            var idString = context.Request.Query["Id"];
+            int id = Convert.ToInt32(idString);
+
+            // Проверка на корректность
+            if (id > maxId || id < minId)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Id is incorrect");
+            }
+            else
+            {
+                await request.Invoke(context);
+            }
+        }
+    }
+}
---
+++ Lab1/SomethingIdAndBlaBla/SomethingIdAndBlaBla/UsingPusingMiddleware.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SomethingIdAndBlaBla
+{
+    public static class UsingPusingMiddleware
+    {
+        // Вызываем нами созданный элемент конвеера
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder, 
+            int minId, int maxId)
+        {
+            return builder.UseMiddleware<NewMiddleware>(minId, maxId);
+        }
+    }
+}
