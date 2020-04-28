83a77f7aa835211ff22396b64c15e2ddc2be0477
My name <ermeeva_r@mail.ru>
2020-04-28 14:47:47 +0300

    мы все делаем

---
+++ Lab1/ServicesHTTP/ServicesHTTP/services/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesHTTP.services
+{
+    public class Email: IMessage
+    {
+        public string Send()
+        {
+            return "Email send";
+        }
+    }
+}
---
+++ Lab1/ServicesHTTP/ServicesHTTP/services/Sms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesHTTP.services
+{
+    public class Sms:IMessage
+    {
+        public string Send()
+        {
+            return "Sms send";
+        }
+    }
+}
