2492a6d68a05f40b4e97924e327b04652bad9f6e
My name <ermeeva_r@mail.ru>
2020-04-30 17:05:47 +0300

    на всякий и сюда кинули

---
+++ Lab1/ServicesInMiddleware/ServicesInMiddleware/MyMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesInMiddleware
+{
+    public class MyMath
+    {
+        public int Sum(string mas)
+        {
+            char[] masChar = mas.ToCharArray();
+            int sum = 0;
+
+            foreach(char item in masChar)
+            {
+                sum += Convert.ToInt32(item.ToString());
+            }
+
+            return sum;
+        }
+        
+        public int Multiplication(string mas)
+        {
+            char[] masChar = mas.ToCharArray();
+            int sum = 1;
+
+            foreach(char item in masChar)
+            {
+                sum *= Convert.ToInt32(item.ToString());
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Lab1/ServicesInMiddleware/ServicesInMiddleware/MyMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesInMiddleware
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, MyMath method)
+        {
+            var tokenSum = context.Request.Query["sum"];
+            var tokenMultiplication = context.Request.Query["multiplication"];
+
+            string masSum = tokenSum.ToString();
+            string masMultiplication = tokenMultiplication.ToString();
+
+            if (masSum != "")
+            {
+                await context.Response.WriteAsync("Sum = " + method.Sum(masSum));
+            }
+            else if (masMultiplication != "")
+            {
+                await context.Response.WriteAsync("Multiplication = " + method.Multiplication(masMultiplication));
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+    }
+}
